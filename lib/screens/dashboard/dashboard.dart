85b9e28d479630b216852400260fa77c3e8771bc
WilliamRWalsh <williamrobertsonwalsh@gmail.com>
2021-10-03 15:05:58 -0400

    nav menu ui

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -6,7 +6,19 @@ import 'package:irl_mobile/screens/dashboard/local_widgets/edit_goal_dialog.dart
 import 'package:irl_mobile/screens/dashboard/local_widgets/ledger.dart';
 import 'package:provider/provider.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  int _selectedIndex = 1;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -33,95 +45,111 @@ class Dashboard extends StatelessWidget {
               body: Padding(
                 padding:
                     const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                child: Container(
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Flexible(
-                          flex: 1,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'Calorie Goal: ',
-                                style: Theme.of(context).textTheme.bodyText2,
-                              ),
-                              Text(
-                                '$goal',
-                                style: Theme.of(context).textTheme.bodyText1,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: GestureDetector(
-                                  onTap: () => showDialog(
-                                      context: context,
-                                      builder: (BuildContext context) {
-                                        return EditGoalDialog();
-                                      }),
-                                  child: CircleAvatar(
-                                    backgroundColor: Colors.yellow,
-                                    radius: 15,
-                                    child: Icon(
-                                      Icons.edit_rounded,
-                                      color: Colors.black,
-                                    ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Flexible(
+                        flex: 1,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Calorie Goal: ',
+                              style: Theme.of(context).textTheme.bodyText2,
+                            ),
+                            Text(
+                              '$goal',
+                              style: Theme.of(context).textTheme.bodyText1,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: GestureDetector(
+                                onTap: () => showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return EditGoalDialog();
+                                    }),
+                                child: CircleAvatar(
+                                  backgroundColor: Colors.yellow,
+                                  radius: 15,
+                                  child: Icon(
+                                    Icons.edit_rounded,
+                                    color: Colors.black,
                                   ),
                                 ),
-                              )
-                            ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Text(
+                          'Remaining Calories',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText2
+                              .copyWith(decoration: TextDecoration.underline),
+                        ),
+                      ),
+                      Flexible(
+                        flex: 4,
+                        child: Text(
+                          "$remainingCalories",
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText1
+                              .copyWith(fontSize: 100),
+                        ),
+                      ),
+                      Flexible(
+                        fit: FlexFit.tight,
+                        flex: 8,
+                        child: Ledger(),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: Size(195, 80),
+                            primary: Theme.of(context).buttonColor,
+                            onPrimary: Theme.of(context).primaryColorDark,
+                            textStyle: Theme.of(context).textTheme.button,
                           ),
+                          onPressed: () => showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AddCaloriesDialog();
+                              }),
+                          child: Text('Add Calories'),
                         ),
-                        Flexible(
-                          flex: 1,
-                          child: Text(
-                            'Remaining Calories',
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyText2
-                                .copyWith(decoration: TextDecoration.underline),
-                          ),
-                        ),
-                        Flexible(
-                          flex: 4,
-                          child: Text(
-                            "$remainingCalories",
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyText1
-                                .copyWith(fontSize: 100),
-                          ),
-                        ),
-                        Flexible(
-                          fit: FlexFit.tight,
-                          flex: 8,
-                          child: Ledger(),
-                        ),
-                        Flexible(
-                          flex: 1,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              minimumSize: Size(195, 80),
-                              primary: Theme.of(context).buttonColor,
-                              onPrimary: Theme.of(context).primaryColorDark,
-                              textStyle: Theme.of(context).textTheme.button,
-                            ),
-                            onPressed: () => showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return AddCaloriesDialog();
-                                }),
-                            child: Text('Add Calories'),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 14,
-                        )
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
+              ),
+              bottomNavigationBar: BottomNavigationBar(
+                backgroundColor: Colors.black,
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.history),
+                    label: 'History',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.settings),
+                    label: 'Settings',
+                  ),
+                ],
+                currentIndex: _selectedIndex,
+                selectedItemColor: Theme.of(context).primaryColor,
+                unselectedItemColor: Colors.white,
+                onTap: _onItemTapped,
               ),
             );
           },
